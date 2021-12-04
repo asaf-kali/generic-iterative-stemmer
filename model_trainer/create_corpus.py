42e851796270c7a9284c39c3cb3bf749dedc2d0e
Asaf Kali <akali93@gmail.com>
2021-12-04 13:43:12 +0200

    Auto commit

--- model_trainer/create_corpus.py
+++ model_trainer/create_corpus.py
@@ -3,23 +3,23 @@ import logging
 from gensim.corpora import WikiCorpus
 
 from model_trainer.config import get_data
+from utils.logging import measure_time
 
 log = logging.getLogger(__name__)
 
 
+@measure_time
 def generate_wiki_corpus_file(articles_file_path: str, output_file_path: str):
     log.info("Creating wiki corpus")
     article_count = 0
     with open(output_file_path, "w") as corpus_file:
-        # lemmatize=False,
         wiki = WikiCorpus(fname=articles_file_path, dictionary={})
         for article_words in wiki.get_texts():
             article = " ".join(article_words)
-            article_encoded = article.encode("utf-8")
-            corpus_file.write(f"{article_encoded}\n")
+            corpus_file.write(f"{article}\n")
             article_count += 1
             if article_count % 1000 == 0:
-                log.info(f"Saved {article_count} articles")
+                log.debug(f"Saved {article_count} articles")
     log.info(f"Finished - Saved {article_count} articles")
 
 
