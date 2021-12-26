9ea3dfbc4a79eb9ea646f6934161b588ef8b0509
Asaf Kali <akali93@gmail.com>
2021-12-27 00:35:13 +0200

    WIP.

--- generic_iterative_stemmer/training/stemming/w2v_stemming_trainer.py
+++ generic_iterative_stemmer/training/stemming/w2v_stemming_trainer.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
 
     corpus_name = "wiki-he-filter"
     corpus_folder = get_path(corpus_name)
-    trainer = Word2VecStemmingTrainer(corpus_folder=corpus_folder, max_iterations=20, completed_iterations=10)
+    trainer = Word2VecStemmingTrainer(corpus_folder=corpus_folder, max_iterations=15, completed_iterations=10)
     trainer.train()
