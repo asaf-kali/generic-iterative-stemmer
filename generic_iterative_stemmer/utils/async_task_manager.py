29941a983948f9aebf46f31be7be99282c8eae45
Asaf Kali <akali93@gmail.com>
2021-12-29 22:51:25 +0200

    Async task manager work.

--- generic_iterative_stemmer/utils/async_task_manager.py
+++ generic_iterative_stemmer/utils/async_task_manager.py
@@ -1,55 +1,116 @@
+import logging
 from queue import Queue
 from threading import Thread
-from typing import Any, Callable, Iterable, Mapping, Optional
+from typing import Any, Callable, ContextManager, Iterable, Mapping, Optional
+
+log = logging.getLogger(__name__)
+STOP = -1
+STOP_TASK = (
+    STOP,
+    STOP,
+    STOP,
+)
 
 
-class AsyncTaskManager:
+class AsyncTaskManager(ContextManager):
+    """
+    Multithreaded task manager.
+    Not that the manager itself is not thread safe - meaning it can be used only in a single thread.
+    """
+
     def __init__(self, workers_amount: int = 5, iter_timeout: Optional[float] = 3):
         super().__init__()
         self.iter_timeout = iter_timeout
         self._task_queue: Queue = Queue()
         self._result_queue: Queue = Queue()
         self._total_task_count = 0
+        self._workers_count = 0
         self.start_workers(workers_amount)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.is_done:
+        if self.is_work_done and self.is_empty:
             raise StopIteration()
         return self.get_result(timeout=self.iter_timeout)
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.__del__()
+
+    def __del__(self):
+        self.join()
+
     @property
-    def is_done(self) -> bool:
-        return self._task_queue.unfinished_tasks == 0 and self._result_queue.unfinished_tasks == 0
+    def is_work_done(self) -> bool:
+        """
+        :return: True iff all tasks processing is done.
+        """
+        return self._task_queue.unfinished_tasks == 0
+
+    @property
+    def is_empty(self) -> bool:
+        """
+        :return: True iff all results fetching is done.
+        """
+        return self._result_queue.unfinished_tasks == 0
+
+    @property
+    def is_closed(self) -> bool:
+        """
+        :return: True iff all worker threads are closed.
+        """
+        return self._workers_count == 0
 
     @property
     def total_task_count(self) -> int:
         return self._total_task_count
 
     def start_workers(self, workers_amount: int):
+        log.debug(f"Starting {workers_amount} workers.")
         for i in range(workers_amount):
             thread = Thread(target=self._work, daemon=True)
             thread.start()
-
-    def _work(self):
-        while True:
-            func, args, kwargs = self._task_queue.get()
-            result = func(*args, **kwargs)
-            self._result_queue.put(result)
-            self._task_queue.task_done()
+            self._workers_count += 1
 
     def add_task(self, func: Callable, args: Iterable = None, kwargs: Mapping = None):
+        if self.is_closed:
+            return
         args = tuple(args) if args else ()
         kwargs = kwargs or {}
         self._total_task_count += 1
         self._task_queue.put((func, args, kwargs))
+
+    def _work(self):
+        log.debug("Worker starting.")
+        while True:
+            func, args, kwargs = self._task_queue.get()
+            if func == STOP:
+                self._task_queue.task_done()
+                break
+            result = func(*args, **kwargs)
+            self._result_queue.put(result)
+            self._task_queue.task_done()
+        log.debug("Worker done.")
 
     def get_result(self, timeout: Optional[float] = 3) -> Any:
         result = self._result_queue.get(block=True, timeout=timeout)
         self._result_queue.task_done()
         return result
 
+    def stop_workers(self, amount: int = None):
+        if amount is None:
+            amount = self._workers_count
+        log.debug(f"Stopping {amount} workers.")
+        for i in range(amount):
+            self._task_queue.put(STOP_TASK)
+            self._workers_count -= 1
+
     def join(self):
-        self._task_queue.join()
+        log.debug("Joining all workers...")
+        self._task_queue.join()  # Wait until all work is done
+        self.stop_workers()
+        self._task_queue.join()  # Wait until all workers are dead
--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -1,10 +1,15 @@
+import logging
 from queue import Empty
 from random import random
 from time import sleep
 
 import pytest
 
+from generic_iterative_stemmer.utils import configure_logging
 from generic_iterative_stemmer.utils.async_task_manager import AsyncTaskManager
+
+log = logging.getLogger(__name__)
+configure_logging()
 
 
 def dummy_task(x: int, duration: float = None):
@@ -17,17 +22,49 @@ def dummy_task(x: int, duration: float = None):
 def test_manager_is_results_iterable():
     manager = AsyncTaskManager(workers_amount=5)
     inputs = set(range(20))
-
     for i in inputs:
         manager.add_task(dummy_task, args=(i,))
 
+    manager.join()
     results = set(manager)
 
     assert results == inputs
-    assert manager.is_done
+    assert manager.is_work_done
+    assert manager.is_empty
 
 
-def test_manager_raises_if_no_results_after_timeout():
+def test_manager_is_a_context_manager():
+    with AsyncTaskManager(workers_amount=5) as manager:
+        inputs = set(range(20))
+        for i in inputs:
+            manager.add_task(dummy_task, args=(i,))
+        results = set(manager)
+
+        assert results == inputs
+        assert manager.is_work_done
+        assert manager.is_empty
+        assert not manager.is_closed
+    assert manager.is_closed
+
+
+def test_manager_raises_empty_exception_if_no_results_after_timeout():
     manager = AsyncTaskManager(workers_amount=1)
     with pytest.raises(Empty):
         _ = manager.get_result(timeout=0.05)
+    manager.join()
+
+
+def test_no_tasks_are_processed_after_manager_joined():
+    manager = AsyncTaskManager(workers_amount=5)
+    inputs = set(range(50))
+    for i in inputs:
+        manager.add_task(dummy_task, args=(i,))
+    manager.join()
+    assert manager.is_closed
+
+    manager.add_task(dummy_task, args=(999,))
+    assert manager.is_work_done
+    assert not manager.is_empty
+    results = set(manager)
+    assert manager.is_empty
+    assert results == inputs
