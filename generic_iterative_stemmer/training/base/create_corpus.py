6749d669dfd8eda64b899d8cd26b1f6c17f5cf61
Asaf Kali <akali93@gmail.com>
2022-01-17 22:34:31 +0200

    Hebrew tokenizer improvements.

--- generic_iterative_stemmer/training/base/create_corpus.py
+++ generic_iterative_stemmer/training/base/create_corpus.py
@@ -1,15 +1,15 @@
 import logging
 import re
-from typing import List
+from typing import Callable, List
 
 from gensim.corpora import WikiCorpus
-from gensim.corpora.wikicorpus import tokenize as gensim_tokenizer
-
-from ...utils import measure_time
+from gensim.corpora.wikicorpus import tokenize
+from tqdm import tqdm
 
 log = logging.getLogger(__name__)
 
-HEBREW_PATTERN = re.compile(r"[\u0590-\u05FF \-']+")
+HEBREW_WORD_PATTERN = re.compile(r"[\u0590-\u05FF\-']+")
+HEBREW_WIKI_REPLACE_PATTERN = re.compile(r"[\u0591-\u05C7]|'''")
 
 SUFFIX_LETTER_TO_NON_SUFFIX_LETTER = {
     "ך": "כ",
@@ -21,10 +21,6 @@ SUFFIX_LETTER_TO_NON_SUFFIX_LETTER = {
 HEBREW_SUFFIX_LETTERS = tuple(SUFFIX_LETTER_TO_NON_SUFFIX_LETTER.keys())
 
 
-def _is_only_hebrew(token: str) -> bool:
-    return HEBREW_PATTERN.fullmatch(token) is not None
-
-
 def _replace_to_non_suffix_letter(token: str) -> str:
     if not token.endswith(HEBREW_SUFFIX_LETTERS):
         return token
@@ -33,29 +29,29 @@ def _replace_to_non_suffix_letter(token: str) -> str:
 
 
 def hebrew_tokenizer(content: str, token_min_len: int, token_max_len: int, lower: bool = False) -> List[str]:
-    tokens = gensim_tokenizer(content, token_min_len, token_max_len, lower)
-    hebrew_tokens = [_replace_to_non_suffix_letter(token) for token in tokens if _is_only_hebrew(token)]
+    content_without_scores = HEBREW_WIKI_REPLACE_PATTERN.sub("", content)
+    tokens = [match.group() for match in HEBREW_WORD_PATTERN.finditer(content_without_scores)]
+    tokens_filtered = filter(lambda token: token_min_len <= len(token) <= token_max_len, tokens)
+    hebrew_tokens = [_replace_to_non_suffix_letter(token) for token in tokens_filtered]
     return hebrew_tokens
 
 
-@measure_time
-def generate_wiki_corpus_file(articles_file_path: str, output_file_path: str):
+def generate_wiki_corpus_file(articles_file_path: str, output_file_path: str, tokenizer_func: Callable = tokenize):
     log.info("Creating wiki corpus")
     article_count = 0
     with open(output_file_path, "w") as corpus_file:
-        wiki = WikiCorpus(fname=articles_file_path, dictionary={}, tokenizer_func=hebrew_tokenizer)
-        for article_words in wiki.get_texts():
+        wiki = WikiCorpus(fname=articles_file_path, dictionary={}, tokenizer_func=tokenizer_func)
+        for article_words in tqdm(wiki.get_texts(), desc="Generate wiki corpus", smoothing=0.1):
             article = " ".join(article_words)
             corpus_file.write(f"{article}\n")
             article_count += 1
-            if article_count % 1000 == 0:
-                log.debug(f"Saved {article_count} articles")
     log.info(f"Finished - Saved {article_count} articles")
 
 
 if __name__ == "__main__":
-    from ...utils import get_path
+    from ...utils import configure_logging, get_path
 
+    configure_logging()
     articles = get_path("hewiki-latest-pages-articles.xml.bz2")
-    out = get_path("wiki-he-filtered2", "corpus.txt")
-    generate_wiki_corpus_file(articles, out)
+    out = get_path("wiki-he", "corpus.txt")
+    generate_wiki_corpus_file(articles, out, tokenizer_func=hebrew_tokenizer)
--- tests/test_create_corpus.py
+++ tests/test_create_corpus.py
@@ -2,7 +2,17 @@ from generic_iterative_stemmer.training.base.create_corpus import hebrew_tokeniz
 
 
 def test_hebrew_tokenizer():
-    content = "מישהו לקח לי את הפנינים, אלוף תן לי מיץ strawberry באמא'שך"
-    expected_tokens = ["מישהו", "לקח", "לי", "את", "הפנינימ", "אלופ", "תנ", "לי", "מיצ", "באמא", "שכ"]
+    content = "מישהו לקח פנינים, י אלוף תן מיץ strawberry באמא'שך. מָתֵמָטִיקָה בבית-ספר"
+    expected_tokens = [
+        "מישהו",
+        "לקח",
+        "פנינימ",
+        "אלופ",
+        "תנ",
+        "מיצ",
+        "באמא'שכ",
+        "מתמטיקה",
+        "בבית-ספר",
+    ]
     tokenized_content = hebrew_tokenizer(content, token_min_len=2, token_max_len=20)
     assert tokenized_content == expected_tokens
