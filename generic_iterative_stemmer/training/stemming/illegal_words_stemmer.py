60a94237b70293857f595c60a40bb5987852d370
Asaf Kali <akali93@gmail.com>
2022-01-17 23:38:13 +0200

    Stemming infra refactoring.

--- generic_iterative_stemmer/training/stemming/illegal_words_stemmer.py
+++ generic_iterative_stemmer/training/stemming/illegal_words_stemmer.py
@@ -9,17 +9,17 @@ log = logging.getLogger(__name__)
 
 
 class IllegalWordsStemmer(StemGenerator):
-    def __init__(self, model: KeyedVectors, legal_words: Iterable[str], workers_amount: int = 5):
-        super().__init__(model=model, workers_amount=workers_amount)
+    def __init__(self, legal_words: Iterable[str], workers_amount: int = 5):
+        super().__init__(workers_amount=workers_amount)
         self.legal_words = set(legal_words)
 
     def is_legal(self, word: str) -> bool:
         return word in self.legal_words
 
-    def find_word_inflections(self, word: str) -> StemDict:
+    def find_word_inflections(self, model: KeyedVectors, word: str) -> StemDict:
         if self.is_legal(word):
             return {}
-        similarities = self.model.most_similar(word, topn=5)
+        similarities = model.most_similar(word, topn=5)
         first_similarity = similarities[0]
         return {word: first_similarity[0]}
 
