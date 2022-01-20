47fe89134b7afc9b3af548aca6cc6c7e157ccf9c
Asaf Kali <akali93@gmail.com>
2022-01-21 00:41:19 +0200

    WIP.

--- notebooks/program_trainer.py
+++ notebooks/program_trainer.py
@@ -1,4 +1,4 @@
-from generic_iterative_stemmer.training.stemming import Word2VecStemmingTrainer
+from generic_iterative_stemmer.training.stemming import FastTextStemmingTrainer
 from generic_iterative_stemmer.training.stemming.default_stem_generator import (
     DefaultStemGenerator,
 )
@@ -11,31 +11,31 @@ from generic_iterative_stemmer.utils import configure_logging, get_path
 def main():
     configure_logging()
 
-    corpus_name = "wiki-he-cbow-2"
+    corpus_name = "wiki-he-ft"
     corpus_folder = get_path(corpus_name)
     training_program = [
-        IterationProgram(stem_generator=DefaultStemGenerator(min_cosine_similarity=0.85, max_edit_distance=0)),
         IterationProgram(stem_generator=DefaultStemGenerator(min_cosine_similarity=0.80, max_edit_distance=0)),
-        IterationProgram(
-            stem_generator=DefaultStemGenerator(
-                min_cosine_similarity=0.75, max_edit_distance=1, min_cosine_similarity_for_edit_distance=0.85
-            )
-        ),
-        IterationProgram(
-            stem_generator=DefaultStemGenerator(
-                min_cosine_similarity=0.75, max_edit_distance=2, min_cosine_similarity_for_edit_distance=0.80
-            )
-        ),
         IterationProgram(stem_generator=DefaultStemGenerator(min_cosine_similarity=0.75, max_edit_distance=0)),
         IterationProgram(
             stem_generator=DefaultStemGenerator(
                 min_cosine_similarity=0.75, max_edit_distance=1, min_cosine_similarity_for_edit_distance=0.80
             )
         ),
+        IterationProgram(
+            stem_generator=DefaultStemGenerator(
+                min_cosine_similarity=0.75, max_edit_distance=1, min_cosine_similarity_for_edit_distance=0.75
+            )
+        ),
+        IterationProgram(stem_generator=DefaultStemGenerator(min_cosine_similarity=0.71, max_edit_distance=0)),
+        IterationProgram(
+            stem_generator=DefaultStemGenerator(
+                min_cosine_similarity=0.71, max_edit_distance=2, min_cosine_similarity_for_edit_distance=0.75
+            )
+        ),
         IterationProgram(stem_generator=DefaultStemGenerator(min_cosine_similarity=0.75, max_edit_distance=0)),
     ]
 
-    trainer = Word2VecStemmingTrainer(corpus_folder=corpus_folder, max_iterations=10, training_program=training_program)
+    trainer = FastTextStemmingTrainer(corpus_folder=corpus_folder, max_iterations=10, training_program=training_program)
     trainer.train()
 
 
