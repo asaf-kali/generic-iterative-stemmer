c96e6b1130a6ca54fc4395aadf50d0072c5cc7b3
Asaf Kali <akali93@gmail.com>
2022-05-06 18:28:23 +0300

    Stemming: add `allowed_words`, some refactorings.

--- generic_iterative_stemmer/training/stemming/corpus_stemmer.py
+++ generic_iterative_stemmer/training/stemming/corpus_stemmer.py
@@ -16,10 +16,10 @@ class StemSentenceResult(BaseModel):
 
 
 class StemCorpusResult(BaseModel):
-    unique_stem_count: int
+    unique_stem_count: int = 0
     unique_word_count: int = 0
-    total_stem_count: int = 0
     total_word_count: int = 0
+    total_stem_count: int = 0
 
     def add(self, stem_sentence_result: StemSentenceResult):
         self.total_word_count += stem_sentence_result.total_word_count
@@ -27,15 +27,19 @@ class StemCorpusResult(BaseModel):
 
     @property
     def unique_stemming_ratio(self) -> float:
+        # The ratio of resulting unique stems to unique words in the corpus.
         return self.unique_stem_count / self.unique_word_count if self.unique_word_count else 0
 
     @property
     def total_stemming_ratio(self) -> float:
+        # The ratio of total stemming to total words in the corpus.
         return self.total_stem_count / self.total_word_count if self.total_word_count else 0
 
 
-def stem_corpus(original_corpus_path: str, output_corpus_path: str, stem_dict: StemDict):
-    stemmer = CorpusStemmer(stem_dict=stem_dict)
+def stem_corpus(
+    original_corpus_path: str, output_corpus_path: str, stem_dict: StemDict, allowed_words: Set[str] = None
+):
+    stemmer = CorpusStemmer(stem_dict=stem_dict, allowed_words=allowed_words)
     return stemmer.stem_corpus(
         original_corpus_path=original_corpus_path,
         output_corpus_path=output_corpus_path,
@@ -43,33 +47,37 @@ def stem_corpus(original_corpus_path: str, output_corpus_path: str, stem_dict: S
 
 
 class CorpusStemmer:
-    def __init__(self, stem_dict: StemDict):
+    def __init__(self, stem_dict: StemDict, allowed_words: Set[str] = None):
         self.stem_dict = stem_dict
-        self.unique_stems = set(stem_dict.values())
-        self.unique_words: Set[str] = set()
+        self.stems = set(stem_dict.values())
+        self.result_unique_words: Set[str] = set()
+        self.allowed_words = allowed_words
 
     def stem_sentence(self, sentence: str) -> StemSentenceResult:
         words = sentence.split()
         stemmed_words = [self.stem_dict.get(word, word) for word in words]
-        self.unique_words.update(stemmed_words)
+        if self.allowed_words:
+            stemmed_words = [word for word in stemmed_words if word in self.allowed_words]
+        self.result_unique_words.update(stemmed_words)
         stemmed_sentence = " ".join(stemmed_words)
         if sentence[-1] == "\n":
             stemmed_sentence += "\n"
         stem_result = StemSentenceResult(
             stemmed_sentence=stemmed_sentence,
-            total_word_count=len(words),
-            total_stem_count=sum(1 if x in self.unique_stems else 0 for x in stemmed_words),
+            total_word_count=len(stemmed_words),
+            total_stem_count=sum(1 if x in self.stems else 0 for x in stemmed_words),
         )
         return stem_result
 
     def stem_corpus(self, original_corpus_path: str, output_corpus_path: str) -> StemCorpusResult:
         log.info("Stemming corpus...")
-        stem_corpus_result = StemCorpusResult(unique_stem_count=len(self.unique_stems))
+        stem_corpus_result = StemCorpusResult()
         with open(original_corpus_path) as original_file, open(output_corpus_path, "w") as output_file:
             for sentence in tqdm(original_file, desc="Corpus stemming"):
                 stem_sentence_result = self.stem_sentence(sentence=sentence)
                 stem_corpus_result.add(stem_sentence_result)
                 output_file.write(stem_sentence_result.stemmed_sentence)
-        stem_corpus_result.unique_word_count = len(self.unique_words)
+        stem_corpus_result.unique_stem_count = len(self.stems.intersection(self.result_unique_words))
+        stem_corpus_result.unique_word_count = len(self.result_unique_words)
         log.info("Stemming corpus done")
         return stem_corpus_result
